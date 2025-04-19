4dd15eb5aaec33f814851cacb91467693ce5bcf6
fa <bfalonne823@gmail.com>
2025-04-19 16:42:39 +0200

    Base skeleton and Expense class

---
+++ src/main/java/com/fa/model/Expense.java
@@ -0,0 +1,39 @@
+package com.fa.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Expense {
+    UUID id;
+    String title;
+    double amount;
+    ExpenseCategory category;
+    LocalDate date;
+    String description;
+
+    public Expense(String title, double amount, ExpenseCategory category, LocalDate date, String description) {
+        this.id = UUID.randomUUID();
+        this.title = title;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+        this.description = description;
+    }
+    
+
+    @Override
+    public String toString() {
+        return String.format(
+            "%s | %s | $%.2f | %s | %s",
+            id.toString().substring(0, 8),
+            date,
+            amount,
+            category,
+            title
+        );
+    }
+}
---
+++ src/main/java/com/fa/model/ExpenseCategory.java
@@ -0,0 +1,5 @@
+package com.fa.model;
+
+public class ExpenseCategory {
+    
+}
