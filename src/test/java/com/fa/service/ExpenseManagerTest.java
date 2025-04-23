d7390f74c2a628d95ab61c671f716f1119999f25
fa <bfalonne823@gmail.com>
2025-04-23 08:49:36 +0200

    Test removeExpenseByIndex

--- src/test/java/com/fa/service/ExpenseManagerTest.java
+++ src/test/java/com/fa/service/ExpenseManagerTest.java
@@ -13,22 +13,38 @@ import static org.junit.Assert.*;
 public class ExpenseManagerTest {
 
     private ExpenseManager expenseManager;
+    private Expense expense;
 
     @Before
     public void setup() {
         expenseManager = new ExpenseManager();
-    }
+        expense = new Expense("Lunch", 12.5, ExpenseCategory.FOOD, "Burger");
+        expenseManager.addExpense(expense);
+    }   
 
     @Test
     public void testAddExpense_increasesListSize() {
         int initialSize = expenseManager.getExpensesList().size();
 
-        Expense expense = new Expense("Lunch", 12.5, ExpenseCategory.FOOD, "Burger");
-        expenseManager.addExpense(expense);
+        Expense newExpense = new Expense("Dinner", 15.0, ExpenseCategory.FOOD, "Pizza");
+        expenseManager.addExpense(newExpense);
 
         int newSize = expenseManager.getExpensesList().size();
 
         assertEquals(initialSize + 1, newSize);
     }
+
+    @Test
+        public void testRemoveExpenseByIndex_decreasesListSize() {
+
+        int initialSize = expenseManager.getExpensesList().size();
+
+        expenseManager.removeExpenseByIndex(1); 
+
+        int newSize = expenseManager.getExpensesList().size();
+        assertEquals(initialSize - 1, newSize);
+    }
+
+
 
 }
